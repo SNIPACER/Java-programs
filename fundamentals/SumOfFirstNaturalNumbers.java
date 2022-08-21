2f1090e7fcaad1060f63bc797ae9a7b8ca8a5d1c
Snipacer <92214782+SNIPACER@users.noreply.github.com>
2022-08-21 15:55:03 +0530

    Update SumOfFirstNaturalNumbers.java

--- fundamentals/SumOfFirstNaturalNumbers.java
+++ fundamentals/SumOfFirstNaturalNumbers.java
@@ -3,16 +3,13 @@ package prograjfds;
 import java.util.Scanner;
 
 public class Test1 {
-	
 	/*
 	 * Create a class with a method which can calculate the sum of first n natural numbers
 	 * which are divisible by 3 or 5
 	 * method name:- calculateSum
 	 * Argument:- int
 	 * Return type:- int
-	 * 
 	 */
-
 		int sum;
 		public static void main(String[] args) {
 			try (Scanner sc = new Scanner(System.in)) {
@@ -25,9 +22,7 @@ public class Test1 {
 				if(i%3==0 || i%5==0) {
 					sum+=i;
 				}
-				
 			}
 			return sum;
 		}
 	}
-
